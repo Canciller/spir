569467b8e103f55f6a36cb5237818366eb996ca3
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-04-13 13:56:46 -0500

    Created base layout

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Sidebar.css'
+
+export default class Layout extends Component {
+    render()  {
+        return (
+            <div className='Sidebar'>
+                Sidebar
+            </div>
+        )
+    }
+}
---
+++ src/views/Reader.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Reader.css';
+
+export default class Reader extends Component {
+    render() {
+        return (
+            <div className='Reader view'>
+                Reader
+            </div>
+        )
+    }
+}
