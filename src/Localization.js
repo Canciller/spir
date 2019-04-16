18b0c63524d6683fed0c140ccd5b79085927e99c
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-04-16 00:45:03 -0500

    Created InitializeLocalize.js

--- src/Localization.js
+++ src/Localization.js
@@ -1,45 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import { LocalizeProvider } from 'react-localize-redux';
-
-import { renderToStaticMarkup } from 'react-dom/server';
-import { withLocalize } from 'react-localize-redux';
-import globalTranslations from './constants/translations/global.json';
-import languages from './constants/translations/languages.json';
-
-const onMissingTranslation = ({ defaultTranslation }) => 'undefined';
-
-class Initialize extends Component {
-    constructor(props) {
-        super(props);
-
-        props.initialize({
-            languages: languages,
-            translation: globalTranslations,
-            options: {
-                renderToStaticMarkup,
-                onMissingTranslation
-            }
-        });
-    }
-
-    render() {
-        return (
-            <Fragment>
-                {this.props.children}
-            </Fragment>
-        )
-    }
-}
-
-const LocalizeInitialize = withLocalize(Initialize);
+import InitializeLocalize from './InitializeLocalize';
 
 export default class Localization extends Component {
     render() {
         return (
             <LocalizeProvider>
-                <LocalizeInitialize>
+                <InitializeLocalize>
                     {this.props.children}
-                </LocalizeInitialize>
+                </InitializeLocalize>
             </LocalizeProvider>
         )
     }
