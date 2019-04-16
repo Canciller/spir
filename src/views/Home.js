aa2367ceb91ed4f77ec7f5261fec46065838aa26
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-04-16 00:46:38 -0500

    Added title

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,14 @@
 import React, { Component, Fragment } from 'react';
+import { Translate } from 'react-localize-redux';
+import Typography from '@material-ui/core/Typography';
 
 export default class Home extends Component {
     render() {
         return (
             <Fragment>
-                Home
+                <Typography variant='title'>
+                    <Translate id='section.checkout' />
+                </Typography>
             </Fragment>
         )
     }
--- src/views/Staff.js
+++ src/views/Staff.js
@@ -1,10 +1,14 @@
 import React, { Component, Fragment } from 'react';
+import { Translate } from 'react-localize-redux';
+import Typography from '@material-ui/core/Typography';
 
 export default class Staff extends Component {
     render() {
         return (
             <Fragment>
-                Staff
+                <Typography variant='title'>
+                    <Translate id='section.staff' />
+                </Typography>
             </Fragment>
         )
     }
