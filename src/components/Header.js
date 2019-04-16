641600ca3204b960a93b8d6355c2f523fe534590
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-04-16 00:47:07 -0500

    Capitalize title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Translate } from 'react-localize-redux';
 import Typography from '@material-ui/core/Typography';
 
 export default class Header extends Component {
     render()  {
         return (
             <Typography variant='headline' color='inherit' noWrap>
-                Spir
+                SPIR
             </Typography>
         )
     }
