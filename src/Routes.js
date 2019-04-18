5b21cfd1e77ef6ee0a427ab842d9c6dd98ef9899
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-04-18 02:01:37 -0500

    Added AddItem route

--- src/Routes.js
+++ src/Routes.js
@@ -4,18 +4,23 @@ import { Switch, Route } from 'react-router-dom';
 import routes from './constants/routes.json';
 
 import Home from './views/Home';
-import Inventory from './views/Inventory';
+import Inventory, { AddItem } from './views/inventory';
 import Staff from './views/Staff';
 import { Cards } from './views/partners';
 import { Reader, Language } from './views/settings';
 
 export default () => (
     <Switch>
+        <Route path={routes.ADDITEM} render={ props => <AddItem {...props} /> } />
+        <Route path={routes.INVENTORY} component={Inventory} />
+
         <Route path={routes.READERS} component={Reader} />
         <Route path={routes.LANGUAGE} component={Language} />
+
         <Route path={routes.CARDS} component={Cards} />
+
         <Route path={routes.STAFF} component={Staff} />
-        <Route path={routes.INVENTORY} component={Inventory} />
+
         <Route path={routes.HOME} component={Home} />
     </Switch>
 )
