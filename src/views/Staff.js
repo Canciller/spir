94adb613dbee063a972bc817cd9921e9a1e7544a
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-04-18 02:10:26 -0500

    Modified Staff.js

--- src/views/Staff.js
+++ src/views/Staff.js
@@ -5,11 +5,8 @@ import Typography from '@material-ui/core/Typography';
 export default class Staff extends Component {
     render() {
         return (
-            <Fragment>
-                <Typography variant='title'>
-                    <Translate id='section.staff' />
-                </Typography>
-            </Fragment>
+            <View title={<Translate id='section.staff' />}>
+            </View>
         )
     }
 }
