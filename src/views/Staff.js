9afa944548021dd2be47a2b39d13b1a4bef5f1e2
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-04-18 13:38:55 -0500

    Fixed missing import

--- src/views/Staff.js
+++ src/views/Staff.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { Translate } from 'react-localize-redux';
 import Typography from '@material-ui/core/Typography';
+import View from '../components/View';
 
 export default class Staff extends Component {
     render() {
