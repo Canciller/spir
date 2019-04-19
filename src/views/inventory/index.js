37b9d97aa9c0b2fa3ffd57c1882dbe9f3f54a4f9
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-04-18 20:29:22 -0500

    Created inventory demo

--- src/views/inventory/index.js
+++ src/views/inventory/index.js
@@ -5,6 +5,7 @@ import { withStyles } from '@material-ui/core/styles';
 
 import Fab from '@material-ui/core/Fab';
 import Link from '@material-ui/core/Link';
+import Grow from '@material-ui/core/Grow';
 
 import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
@@ -17,9 +18,11 @@ import routes from '../../constants/routes.json';
 
 import AddItem from './AddItem';
 
+import { withSpirApi } from '../../util';
+
 const styles = theme => ({
     absolute: {
-        position: 'absolute',
+        position: 'fixed',
         bottom: theme.spacing.unit * 2,
         right: theme.spacing.unit * 2,
     },
@@ -29,22 +32,41 @@ const styles = theme => ({
 })
 
 class Inventory extends Component {
+    state = {
+        items: []
+    }
+
+    componentDidMount() {
+        const { spirApi } = this.props;
+        spirApi.inventory.get(items => {
+            this.setState({ items });
+        });
+    }
+
     render() {
         const { classes } = this.props;
 
         return (
             <View title={<Translate id='section.inventory' />}>
-                <Card title='Title' description='Description' />
-                <div className={classes.absolute}>
-                    <IconButton aria-label="Delete">
-                        <DeleteIcon />
-                    </IconButton>
-                    <Link component={RouterLink} to={routes.ADDITEM}>
-                        <Fab color='primary' className={classes.add}>
-                            <AddIcon />
-                        </Fab>
-                    </Link>
-                </div>
+                { this.state.items.map((item, i) => {
+                    let { name, description, price } = item;
+                    if(!description || description === '') description = 'No Description';
+                    return (
+                        <Card key={item.id} title={name} description={description} />
+                    )
+                }) }
+                <Grow in={true}>
+                    <div className={classes.absolute}>
+                        <IconButton aria-label="Delete">
+                            <DeleteIcon />
+                        </IconButton>
+                        <Link component={RouterLink} to={routes.ADDITEM}>
+                            <Fab color='primary' className={classes.add}>
+                                <AddIcon />
+                            </Fab>
+                        </Link>
+                    </div>
+                </Grow>
             </View>
         )
     }
@@ -54,4 +76,4 @@ export {
     AddItem
 }
 
-export default withStyles(styles)(Inventory);
+export default withStyles(styles)(withSpirApi(Inventory));
