f22359b614a5aa0b98ea5938e45b370aaf5674ff
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-04-18 20:28:28 -0500

    Added AddItem.js

--- src/views/inventory/AddItem.js
+++ src/views/inventory/AddItem.js
@@ -1,37 +1,43 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { Translate } from 'react-localize-redux';
 import { withStyles } from '@material-ui/core/styles';
-import FormControl from '@material-ui/core/FormControl';
+
 import View from '../../components/View';
 import Form from '../../components/Form';
 import Select from '../../components/Select';
 import TextField from '../../components/TextField';
 import Button from '../../components/Button';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
 
-const styles = theme => ({
-    margin: {
-        marginTop: theme.spacing.unit * 2
-    }
-})
+import { withSpirApi } from '../../util';
+
+const styles = theme => ({})
 
 class AddItem extends Component {
     state = {
         name: '',
+        nameEmpty: false,
+
         description: '',
+
         price: 0,
-        category: 0,
+        priceNaN: false,
+
+        category_code: 0,
         categories: [
             { value: 0, name: 'Product' },
             { value: 1, name: 'Service' }
         ],
-        priceNaN: false
+    }
+
+    formHasError = () => {
+        const { nameEmpty, priceNaN } = this.state;
+        return nameEmpty || priceNaN;
     }
 
     handleNameChange = e => {
-        this.setState({ name: e.target.value });
+        let value = e.target.value;
+        this.setState({ name: value, nameEmpty: value === '' });
     }
 
     handleDescriptionChange = e => {
@@ -44,83 +50,76 @@ class AddItem extends Component {
 
         if(isNaN(price)) return this.setState({ priceNaN: true });
 
-        this.setState({ price, priceNaN: false });
+        this.setState({ price, priceNaN: price <= 0 });
     }
 
     onChangeCategory = e => {
         let value = e.target.value;
-        this.setState({ category: value });
+        this.setState({ category_code: value });
     }
 
     handleAdd = () => {
-        console.log(this.state);
-    }
+        let { name, description, price, category_code } = this.state;
+        let { spirApi } = this.props;
 
-    componentDidMount() {
-        this.forceUpdate();
+        this.setState({
+            nameEmpty: name === '',
+            priceNaN: price <= 0
+        }, () => {
+            if(!this.formHasError()) {
+                let item = { name, description, price, category_code }
+                spirApi.inventory.add(item);
+            }
+        });
+
     }
 
     render() {
-        const {
-            classes,
-            history
-        } = this.props;
+        const { classes } = this.props;
 
         const {
+            nameEmpty,
             priceNaN,
-            category,
+            category_code,
             categories
         } = this.state;
 
         return (
             <View
-                title='Add Item'
-                history={history}
+                title={<Translate id='section.additem' />}
                 back
             >
                 <Form>
                     <TextField
-                        label='Name'
+                        required
+                        error={nameEmpty}
+                        label={<Translate id='name'/>}
                         onChange={this.handleNameChange}
                     />
                     <TextField
-                        margin
-                        label='Description'
+                        label={<Translate id='description'/>}
+                        placeholder='No description'
                         onChange={this.handleDescriptionChange}
                     />
-                    <FormControl
+                    <TextField
+                        required
                         error={priceNaN}
-                        variant="outlined"
-                        className={classes.margin}
-                    >
-                        <InputLabel
-                            ref={ref => {
-                                this.labelRef = ReactDOM.findDOMNode(ref);
-                            }}
-                            htmlFor="component-outlined"
-                        >
-                            Price
-                        </InputLabel>
-                        <OutlinedInput
-                            onChange={this.handlePriceChange}
-                            id="component-outlined"
-                            labelWidth={this.labelRef ? this.labelRef.offsetWidth : 0}
-                            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                            placeholder='00.00'
-                        />
-                    </FormControl>
+                        label={<Translate id='price'/>}
+                        adorment='$'
+                        placeholder='0.00'
+                        onChange={this.handlePriceChange}
+                    />
                     <Select
-                        margin
                         onChange={this.onChangeCategory}
-                        label='Category'
-                        value={category}
+                        label={<Translate id='category'/>}
+                        value={category_code}
                         items={categories}
                     />
                     <Button
-                        onClick={this.handleAdd}
                         margin
+                        onClick={this.handleAdd}
                     >
-                        Add
+                        <Translate id='add' />
                     </Button>
                 </Form>
             </View>
@@ -128,4 +127,4 @@ class AddItem extends Component {
     }
 }
 
-export default withStyles(styles)(AddItem);
+export default withStyles(styles)(withSpirApi(AddItem));
