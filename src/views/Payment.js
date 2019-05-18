d7e752f8f0dd9c125d4ff14cca84376f90a21619
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-05-17 21:11:42 -0500

    Updated Payment view

--- src/views/Payment.js
+++ src/views/Payment.js
@@ -1,14 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { withSnackbar } from 'notistack';
 import { withStorage } from '../context';
 
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 import FormView from '../components/FormView';
 
 const ipcRenderer = window.require('electron').ipcRenderer;
 
 const styles = theme => ({
+    cart: {
+        display: 'flex',
+        flexDirection: 'column',
+        background: theme.palette.background.paper,
+        marginTop: theme.spacing.unit * 2,
+        borderRadius: theme.shape.borderRadius,
+        border: '1px solid',
+        borderColor: theme.palette.divider,
+        paddingLeft: theme.spacing.unit,
+        paddingRight: theme.spacing.unit
+    },
+    cartItem: {
+        padding: theme.spacing.unit,
+        display: 'flex'
+    },
+    cartItemName: {
+        flex: 1
+    },
+    cartItemQuantity: {
+        paddingRight: theme.spacing.unit * 2
+    },
     info: {
         display: 'flex',
         marginTop: theme.spacing.unit * 2
@@ -24,7 +46,10 @@ class Payment extends Component {
     }
 
     onConfirm = (e, { amount }) => {
-        console.log(amount);
+        const {
+            change
+        } = this.state;
+
     }
 
     componentDidMount() {
@@ -46,9 +71,10 @@ class Payment extends Component {
             amount
         } = this.state;
 
-        const total = storage.cart.total();
+        const total = storage.cart.total(),
+              change = amount - total;
 
-        const change = amount - total;
+        const cart = storage.cart.get();
 
         return (
             <FormView
@@ -85,6 +111,36 @@ class Payment extends Component {
                     }
                 }}
             >
+                <div
+                    className={classes.cart}
+                >
+                    {cart.map((item, i) => {
+                        return (
+                            <Fragment
+                                key={i}
+                            >
+                                <div
+                                    className={classes.cartItem}
+                                >
+                                    <Typography
+                                        variant='subtitle1'
+                                        className={classes.cartItemName}
+                                    >
+                                        {item.name} x {item.quantity}
+                                    </Typography>
+                                    <Typography
+                                        variant='subtitle1'
+                                    >
+                                        {`$${item.price.toFixed(2)}`}
+                                    </Typography>
+                                </div>
+                                { i !== cart.length - 1 &&
+                                    <Divider />
+                                }
+                            </Fragment>
+                        )
+                    })}
+                </div>
                 <div
                     className={classes.info}
                 >
