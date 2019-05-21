27a79fe77c75c3270387b5c5e1db7f6804c18777
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-05-21 15:38:10 -0500

    Fix

--- public/spir/index.js
+++ public/spir/index.js
@@ -79,6 +79,16 @@ class Reader {
         try {
             this.socket = net.createConnection(this.port, this.address);
 
+            this.socket.setTimeout(3000);
+
+            this.socket.on('timeout', () => {
+                this.send('reader:status', status(
+                    false,
+                    'Reader connection timeout'
+                ));
+                this.socket.end();
+            });
+
             this.socket.on('connect', () => {
                 this.socket.setEncoding('utf-8');
 
